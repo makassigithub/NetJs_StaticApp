3dbb1eed27e740893fb62c1aeff8c4cc7ef1cd5c
Brahima Traore <makassi@MacBook-Pro-de-Brahima.local>
2023-04-15 06:08:58 -0400

    add formatting and basic navigation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-function HomePage(){
-    return (
-        <main>
-           <h1>
-                Blog post
-           </h1> 
-        </main>
-    )
+import Link from "next/link";
+
+function HomePage() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>Blog post</h1>
+      </main>
+    </>
+  );
 }
 
 export default HomePage;

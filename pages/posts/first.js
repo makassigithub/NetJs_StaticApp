49524b19c06c29736b7631e32c9e3751c2766dad
Brahima Traore <makassi@MacBook-Pro-de-Brahima.local>
2023-04-15 08:30:41 -0400

    Add static props

--- pages/posts/first.js
+++ pages/posts/first.js
@@ -1,15 +1,26 @@
 import Head from "next/head";
 
-function FirstPost() {
-  console.log("[About] page rendered");
+export async function getStaticProps() {
+  return {
+    props: {
+      post: {
+        title: " My First Post",
+        body: "This is my first loaded static post",
+      },
+    },
+  };
+}
+
+function FirstPost({ post }) {
+  console.log("[Fist Post] page rendered");
   return (
     <>
       <Head>
-        <title>First post - blog</title>
+        <title>{post.title}</title>
       </Head>
       <main>
-        <h1>First post - blog</h1>
-        <p>This is my first Post</p>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
       </main>
     </>
   );

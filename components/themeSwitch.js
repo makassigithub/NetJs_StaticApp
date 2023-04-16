f2ef26b7c0cf129ac53618dec2cb5c1f497f097a
Brahima Traore <makassi@MacBook-Pro-de-Brahima.local>
2023-04-16 08:16:30 -0400

    storing theme in local storage and fixing hydrationWarning issue

--- components/themeSwitch.js
+++ components/themeSwitch.js
@@ -2,11 +2,28 @@ import { useState } from "react";
 import DarkTheme from "./darkTheme";
 
 function ThemeSwitch() {
-  const [darkMode, setDarkMode] = useState(false);
+  const loadDarkMode = () => {
+    if (typeof localStorage === "undefined") {
+      return false;
+    }
+
+    const mode = localStorage.getItem("darkMode");
+    return mode === null ? false : JSON.parse(mode);
+  };
+
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
   const text = darkMode ? "Light Mode" : "Dark Mode";
+
+  const handleClick = () => {
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
-      <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+      <button onClick={handleClick} suppressHydrationWarning>
+        {text}
+      </button>
       <style jsx>{`
         button {
           background: none;
